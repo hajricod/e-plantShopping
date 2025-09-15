5d609270353a4d9fa858676cc3a90fd57d18a8ae
hajricod <48984962+hajricod@users.noreply.github.com>
2025-09-15 09:40:36 +0400

    added decrement and remove cart item

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -27,11 +27,16 @@ const CartItem = ({ onContinueShopping }) => {
     dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
-  const handleDecrement = (item) => {
-   
-  };
+const handleDecrement = (item) => {
+  if (item.quantity > 1) {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+  } else {
+    dispatch(removeItem(item.name));
+  }
+};
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
